47c3ceead2ca6f8c9eb4e7fca71005fdfa365b09
Rachana-018 <158443864+Rachana-018@users.noreply.github.com>
2025-04-25 15:27:49 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+// Theme toggle
+const toggle = document.getElementById("theme-toggle");
+toggle.addEventListener("click", () => {
+  document.body.classList.toggle("dark");
+  toggle.textContent = document.body.classList.contains("dark") ? "☀️" : "🌙";
+});
